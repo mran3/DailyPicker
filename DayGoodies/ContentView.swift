c00ce1c9cdd0bafdba3474595893f45a808fa87f
Andres Acevedo - mran3 <x88andres@hotmail.com>
2019-09-29 01:06:43 -0500

    Initial Commit

---
+++ DayGoodies/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DayGoodies
+//
+//  Created by troquer on 9/29/19.
+//  Copyright © 2019 zourz. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
